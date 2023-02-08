1cd17e8e4e85710bef48c51bb16c46c844b85136
root <root@LAPTOP-P75H5CLD.localdomain>
2023-02-08 11:42:14 +0000

    Added navigation, and fake api to fetch the data

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero
+  constructor(private heroService: HeroService) {}
+  goBack(): void {
+    this.location.back();
+  }
+  save(): void{
+    if (this.hero){
+      this.heroService.updateHero(this.hero)
+       .subscribe(() => this.goBack())
+    }
+  }
+}
